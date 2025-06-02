5bd1d14d014ee38d621ae98c9961e920d61bee8a
vall <>
2025-06-02 11:41:25 +0300

    use a simpler selection of hiding column

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 browser.runtime.onMessage.addListener(function () {
-  let elem = document.getElementById("react_rootVideo_page").firstChild.firstChild.firstChild.childNodes[1];
+  let elem = document.getElementsByClassName("vkuiInternalTwoColumnLayoutColumn")[1];
   let curstyle = window.getComputedStyle(elem).getPropertyValue("display");
   if (curstyle == "none")
     elem.style.display = "block";
